655d6810aeaa1fcb594c5bd44a346564ce836e5f
vagrant <vagrant@localhost.localdomain>
2020-04-11 08:41:42 +0000

    [5-9]layout

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'af72a83975c92094bb52af708faf9cd131cf915cf7c98350aaff50d7144464d906174fcd98ed61856071148e3d14ca1f9ba3b224d62f44ff2a40aa22f9573c37'
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root 'home#index'
+  get 'home/about' => 'home#about'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+end
